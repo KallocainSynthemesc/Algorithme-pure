4c4250d5f15ef9dab1ac7ec38798284c3292fb98
KallocainSynthemesc <74613762+KallocainSynthemesc@users.noreply.github.com>
2022-10-18 16:05:11 +0200

    add poweshell build script

---
+++ generate-presentation.ps1
@@ -0,0 +1,18 @@
+﻿$BackupEncoding = [Console]::OutputEncoding
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$dir = (Get-Location).tostring() + "\build"
+[System.IO.Directory]::CreateDirectory($dir)
+
+$index = $dir + "\index.html"
+inliner index.html | Out-File -FilePath $index
+
+$arr = $dir + "\arrayVisualizer.html"
+inliner arrayVisualizer.html | Out-File -FilePath $arr
+
+$lightswitch = $dir + "\lightswitch.html"
+inliner lightswitch.html | Out-File -FilePath $lightswitch
+
+$time = $dir + "\time.html"
+inliner time.html | Out-File -FilePath $time
+
+[Console]::OutputEncoding = $BackupEncoding
